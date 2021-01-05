1cfd1bcd1e3ed37f2c35c93c50b78718e488f52a
ProGamer29 <52620563+ProGamer29@users.noreply.github.com>
2021-01-05 13:50:19 +0100

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,7 +8,7 @@ from . import ALIVE_NAME, StartTime, catdef, catversion, mention, reply_id
 DEFAULTUSER = ALIVE_NAME or "cat"
 CAT_IMG = Config.ALIVE_PIC
 CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  😏 "
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "   ✥  "
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
